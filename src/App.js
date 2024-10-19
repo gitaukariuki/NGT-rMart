c79d01e4889172edfacf2be5ab22fdc405909aed
gitaukariuki <gitaukariuki@gmail.com>
2024-10-19 12:19:48 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import { Routes, Route } from 'react-router-dom';
 import Products from './components/Products';
 import Product from './components/Product';
 import Cart from './components/Cart';
-//import Category from './components/Category'; // Assuming you have a Category component
+import CartButton from './components/CartButton';
+import About from './components/About';  // Importing the AboutUs component
+import Contacts from './components/Contacts';  // Importing the Contacts component
+import Login from './components/Login';  // Importing the Login component
+import Register from './components/Register';  // Importing the Register component
 
 function App() {  
   return (  
@@ -16,7 +20,10 @@ function App() {
         <Route exact path="/products" element={<Products />} />
         <Route exact path="/products/:id" element={<Product />} />
         <Route exact path="/cart" element={<Cart />} />
-      
+        <Route exact path="/about" element={<About />} />  {/* About Us Route */}
+        <Route exact path="/contacts" element={<Contacts />} />  {/* Contacts Route */}
+        <Route exact path="/login" element={<Login />} />  {/* Login Route */}
+        <Route exact path="/register" element={<Register />} />  {/* Register Route */}
       </Routes>  
     </>  
   );  
